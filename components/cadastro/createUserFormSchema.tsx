6173d13bccc9796290460e6591f31a95730f2606
Lucas Santana <lucasadd15@gmail.com>
2024-02-27 21:27:25 -0300

    alterar cod

--- components/cadastro/createUserFormSchema.tsx
+++ components/cadastro/createUserFormSchema.tsx
@@ -2,8 +2,8 @@
 import { z } from "zod";
 
 export const createUserFormSchema = z.object({
-    id: z.string().min(6, "Seu usuario tem muito pouco Caracteres!"),
-    name: z.string().min(6, "Nome tem muito curto"),
+    id: z.string().min(3, "Seu usuario tem muito pouco Caracteres!"),
+    name: z.string().min(3, "Nome tem muito curto"),
     cpfcnpj: z.string().min(4, "DADOS incompletos!"),
     coins: z.string().min(1,"Minimo de coins é 1")
 });
